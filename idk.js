2934ade3c7c62cec10b68060dc64198de555afe8
Ady_zxxy <161061040+noideafornickk@users.noreply.github.com>
2024-07-23 23:29:49 -0300

    Add files via upload

--- idk.js
+++ idk.js
@@ -1,21 +1,20 @@
 function carregar (){
 var msg = window.document.getElementById('msg')
 var foto = window.document.getElementById('foto')
-var data = 19 //new Date()
+var data =  new Date()
 var hora =  data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 6){
-    foto.src= 'madruga.webp'
-    msg.innerHTML = `Boa Madrugada!`}
-else if (hora >= 6 && hora < 12){
-    foto.src='manha.jpg'
-    msg.innerText ='Bom dia!'
+    img.src= 'madruga.webp'
+    msg.innerHTML = `Agora são ${hora} horas. <br> Boa Madrugada! `
+}else if (hora >= 6 && hora < 12){
+    img.src='manha.jpg'   
+    msg.innerHTML = `Agora são ${hora} horas. <br> Buenos Dias!`
 } else if (hora >= 12 && hora <18) {
-    foto.src = 'tarde.jpg'
-    msg.innerHTML='Boa Tarde!'
+    img.src = 'tarde.jpg'
+    msg.innerHTML = `Agora são ${hora} horas. <br> Boa Tarde!`
 }   
  else {
-    foto.src='noite.png'
-    msg.innerHTML = 'Buenas Noches! '
+    img.src='noite.png'
+   msg.innerHTML = `Agora são ${hora} horas. <br> Buenas Noches!`
 }
 }
